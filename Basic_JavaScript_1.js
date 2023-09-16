c650fd11955ebabfe9720da1e3b94e08e06e6830
jokel1973 <jordan.kelley@gmail.com>
2023-09-15 17:00:22 -0700

    added Basic HTML file
    Basic HTML template

---
+++ Basic_JavaScript_1.js
@@ -0,0 +1,8 @@
+<script>
+	function My_First_Funtion() {
+		var String = "Kiss me, I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getEliemintById("Irish").innerHTML =
+		result;
+	}
+</script>
